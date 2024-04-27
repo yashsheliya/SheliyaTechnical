595205169c166a90bafc68c1084a8260ba29955c
yash sheliya <135209192+yashsheliya@users.noreply.github.com>
2024-04-27 10:58:36 +0530

    sass add in npm install sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Home from './Components/Home';
